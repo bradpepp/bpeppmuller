53865261107adfb718016eb1f2c3cc6d33cdb11d
Bradley Peppmuller <bpeppmuller@csumb.edu>
2018-03-04 00:53:37 +0000

    re-adding lab_4

--- labs/lab_4/index.php
+++ labs/lab_4/index.php
@@ -1,168 +1,223 @@
 <?php
 
- //print_r($_GET); //displaying all content submitted in the form using the GET method
-
-  $backgroundImage = "img/sea.jpg";
+    $backgroundImage = "img/sea.jpg";
     
-  if (isset($_GET['keyword'])) { //if form was submitted
-      
-      include 'api/pixabayAPI.php';
-      
-      echo "<h3>You searched for " . $_GET['keyword'] . "</h3>";
-      
-      $orientation = "horizontal";
-      $keyword = $_GET['keyword'];
-      
-      if (isset($_GET['layout'])) {  //user checked a layout
+    //print_r($_GET); //displaying all content submitted in the form using the GET method
         
-        $orientation = $_GET['layout'];
-        
-      }
-      
-      if (!empty($_GET['category'])) { //user selected a category
-        $keyword = $_GET['category'];
-      }
-      
-      $imageURLs = getImageURLs($keyword, $orientation);
-      
-      //$backgroundImage = $imageURLs[rand(0, count($imageURLs)-1];
-      $backgroundImage = $imageURLs[array_rand($imageURLs)];
-      
-      //print_r($imageURLs);
-      
-      
-  }      
- 
- function checkCategory($category){
-   
-    if ($category == $_GET['category']) {
-       echo " selected";
+    if(empty($_GET['keyword']) && empty($_GET['category']))
+    {
+         echo "<h2> You must type a keyword or select a category </h2>";
     }
- }
+    
+    if((($_GET['keyword'] == "" || empty($_GET['keyword'])) && !empty($_GET['category'])) || (!empty($_GET['keyword']) && !empty($_GET['category'])) || (!empty($_GET['keyword']) && empty($_GET['category'])))
+    {
+        include 'api/pixabayAPI.php';
+        
+        
 
+        $orientation = "horizontal";
+        $keyword = $_GET['keyword'];
+        
+        if(isset($_GET['layout']))
+        {
+            $orientation = $_GET['layout'];
+        }
+        
+        if(!empty($_GET['category']))
+        {
+            $keyword = $_GET['category'];
+        }
+        
+        echo "<h3>You searched for: " . $keyword . "</h3>";
+        
+        $imageURLs = getImageURLs($keyword, $orientation);
+        
+        $backgroundImage = $imageURLs[array_rand($imageURLs)];
+    }
+
+    
+    function checkCategory($category)
+    {
+        if($category == $_GET['category'])
+        {
+            echo " selected";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Lab 4: Pixabay Carousel </title>
+        <title>Image Carousel</title>
     </head>
+    
     <style>
         @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
         @import url("css/styles.css");
-        body {
-            background-image: url(<?=$backgroundImage?>);
-            background-size: 100% 100%;
-                background-attachment: fixed;
+        body 
+        {
+            background-image: url("<?=$backgroundImage?>");
+            background-size:100%;
+            background-repeat: no-repeat;
+            text-align: center;
         }
         
-        #carouselExampleIndicators {
+        #carouselExampleIndicators
+        {
+            margin: 0 auto;
             width: 500px;
-            margin: 0 auto; /*centers a div container*/
+            margin: 0 auto;
+            text-align: center;
         }
-         
+        
+       
+        #searchEngine
+        {
+            border-radius: 20px;
+        }
+        
+        #layoutDiv
+        {
+            display: inline-block; 
+            color: black; 
+            text-align: left; 
+            background-color: white; 
+            padding: 5px; 
+            border-radius: 10px;
+        }
+        
+        #catDiv
+        {
+            display: block;
+            padding: 10px;
+        }
+        
+        #subDiv
+        {
+            display: block;
+            padding: 10px;
+        }
+        
+        #submitButton
+        {
+            border-radius: 50px;
+        }
+        
+        footer
+        {
+            background-color: #FFFFFF;
+            color: black;
+        }
+        
+        h3
+        {
+            background-color: #FFFFFF;
+            color: black;
+        }
+        
     </style>
     <body>
+        <br/><br/>
 
-        <?php
-        
-            if (!isset($_GET['keyword'])) {
-        
-              echo "<h2> You must type a keyword or select a category </h2>";
+        <form method = "GET">
+            <input type = "text"  size = "20" name = "keyword" placeholder = "Keyword" value = "<?= $_GET['keyword']?>"/>
             
-            }  
-        ?>
-
-        
-
-        <form method="GET">
-            
-            <input type="text" size="20" name="keyword" placeholder="Keyword to search for" value="<?=$_GET['keyword']?>"/>
-            
-            <input type="radio" name="layout" value="horizontal" id="hlayout" 
-            
+            <div id = "layoutDiv">
+            <input type = "radio" name = "layout" value = "horizontal" id = "hlayout"
             <?php
-               if ($_GET['layout'] == "horizontal") {
-                 echo "checked";
-               }
+                if($_GET['layout'] == "horizontal")
+                {
+                    echo "checked";
+                }
             ?>
+            class = "radioInput">
+            <label for = "hlayout" > Horizontal </label>
+            </br>
+            <input type = "radio" name = "layout" value = "vertical" id = "vlayout" <?= ($_GET['layout'] == "vertical")?"checked":"" ?> class = "radioInput">
+            <label for = "vlayout" > Vertical </label>
+            </div>
             
-            >
-            <label for="hlayout"> Horizontal </label>
-            
-            <input type="radio" name="layout" value="vertical" id="vlayout" <?= ($_GET['layout']=="vertical")?"checked":"" ?>>
-            <label for="vlayout"> Vertical </label>
-            
-            <select name="category">
-              <option value="" >  Select One </option> 
-              <option value="sea" <?=checkCategory('sea')?>>  Ocean </option>
-              <option <?=checkCategory('Forest')?>>  Forest </option>
-              <option <?=checkCategory('Sky')?>>  Sky </option>
+            <div id = "catDiv">
+            <select name = "category" >
+                <option value = ""> Select One </option>
+                <option <?=checkCategory('Dogs')?>> Dog </option>
+                <option <?=checkCategory('Cats')?>> Cats </option>
+                <option <?=checkCategory('Otters')?>> Otters </option>
+                <option <?=checkCategory('Sea')?>> Sea </option>
+                <option <?=checkCategory('Racecar')?>> Racecar </option>
             </select>
+            </div>
             
+            <div id = "subDiv">
+            <input type = "submit" id = "submitButton" value = "Submit" />
+            </div>
             
-            <input type="submit" value="Submit!"/>
-                    
         </form>
         
+        <!--($_GET['keyword'] == "" || !empty($_GET['keyword'])) || !empty($_GET['category'])-->
         <?php
-          
-           if (isset($_GET['keyword'])) {
-        ?>
+            // echo empty($_GET['category']);
+            if((($_GET['keyword'] == "" || empty($_GET['keyword'])) && !empty($_GET['category'])) || (!empty($_GET['keyword']) && !empty($_GET['category'])) || (!empty($_GET['keyword']) && empty($_GET['category']))) {
         
+        ?>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <?php for ($i=1; $i < 7; $i++) { ?>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="<?=$i?>"></li>
-                <?php } //endFor ?> 
-                <!--
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-                -->
-              </ol>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[3]?>" alt="Forth slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[4]?>" alt="fifth slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[5]?>" alt="Sixth slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="<?=$imageURLs[6]?>" alt="seventh slide">
-                </div>
-              </div>
-              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+            <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[3]?>" alt="Fourth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[4]?>" alt="Fifth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[5]?>" alt="Sixth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[6]?>" alt="Seventh slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
         
         <?php
-            }//endIf
+            } //end of if statement above in php block
         ?>
         
         
-
+        <br/><br/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
     </body>
+    
+    <footer>
+        <hr>
+            CST 336 Internet Programming 2018&copy; Bradley Peppmuller <br />
+            <strong> Disclaimer: </strong> This webpage is used for academic purposes only. <br />
+            
+    </footer>
 </html>
